151d349ea03d52d86716db314d3f19b82c82e462
Cherrise-exe <74623727+Cherrise-exe@users.noreply.github.com>
2020-12-28 09:23:53 -0500

    Update sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,19 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  function odd(n){
+  return Boolean(n % 2)
+}
+
+function findSum(n) {
+  let sum = 0;
+  for(let count = 0; count <= n; count++){
+    if(odd(count)) {
+      sum += count;
+    }
+  }
+  return console.log(sum);
+}
+
 }
 
 // Do not edit this line;
