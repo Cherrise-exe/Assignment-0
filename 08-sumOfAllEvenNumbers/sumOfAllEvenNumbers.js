56b5a6bc66bfd2596c6f8e18ee4f79ecb699bf50
Cherrise-exe <74623727+Cherrise-exe@users.noreply.github.com>
2020-12-28 09:26:26 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,18 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  function isEven(n){
+  return !Boolean(n % 2)
+}
+
+function findSum(n) {
+  let sum = 0;
+  for(let count = 0; count <= n; count++){
+    if(isEven(count)) {
+      sum += count;
+    }
+  }
+  return console.log(sum);
+}
 }
 
 // Do not edit this line;
