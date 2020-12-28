7e980752ca4846e0fb831d2305ec4c7685152162
Cherrise-exe <74623727+Cherrise-exe@users.noreply.github.com>
2020-12-28 00:12:33 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  	var array = title.split(" ");
+	var correctedTitle = array.map(
+		function (value){
+				return value.replace(value.charAt(0), value.charAt(0).toUpperCase());
+		});
+	return correctedTitle.join(" ");
 }
 
 // Do not edit this line;
